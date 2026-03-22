8ad0a6b17c454685fce3ed240e8578d59d23046c
Manohar A <137694627+electromanu@users.noreply.github.com>
2026-03-22 14:34:29 +0530

    Configure app to run on environment-defined port
    Update app to run on specified port from environment variable.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request,redirect
 from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__)
 
@@ -50,5 +51,7 @@ def service():
     return render_template("service.html")
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
